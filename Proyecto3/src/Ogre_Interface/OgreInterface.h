b6889ca058712379ee0c8cdafd7990d066b32e4e
Diego BV <diego.baratto8@gmail.com>
2019-02-26 17:30:13 +0100

    Diego Baratto / Juan Barea : Ventana creada, Ogre funcionando
    Ventana funcionando, no 100% (falta Scene / Input)
    
    Co-Authored-By: jubarea <jubarea@users.noreply.github.com>

--- Proyecto3/src/Ogre_Interface/OgreInterface.h
+++ Proyecto3/src/Ogre_Interface/OgreInterface.h
@@ -1,64 +1,24 @@
 #pragma once
 #include <OgreRoot.h>
 #include <OgreBuildSettings.h>
-//#include <OgreOverlaySystem.h>
-#include <OgreFrameListener.h>
-#include <SDL.h>
+#include <OgreRenderSystem.h>
+#include "OgreRenderWindow.h"
 
-namespace OgreBites
+using namespace Ogre;
+
+class OgreInterface //: public Ogre::FrameListener
 {
-	typedef SDL_Window NativeWindowType;
+private:
+	Ogre::Root* mRoot = nullptr;
+	Ogre::RenderWindow* mWindow = nullptr;
+	RenderSystem* rs = nullptr;
+	void createRoot();
+	bool setConfiguration();
+	void createWindow();
 
-	/**
-	link between a renderwindow and a platform specific window
-	*/
-	struct NativeWindowPair
-	{
-		Ogre::RenderWindow* render = nullptr;
-		NativeWindowType* native = nullptr;
-	};
-
-	class OgreInterface : public Ogre::FrameListener
-	{
-	private:
-		Ogre::Root* root;
-		//Ogre::OverlaySystem* mOverlaySystem;  // Overlay system
-		NativeWindowPair mWindow; // the window
-		Ogre::String wName = "Proyecto 3";
-		virtual void createRoot();
-		virtual void setup();
-		/**
-		Create a new render window
-		You must use SDL and not an auto-created window as SDL does not get the events otherwise.
-		By default the values from ogre.cfg are used for w, h and miscParams.
-		*/
-		virtual NativeWindowPair createWindow(const Ogre::String& name);
-		void setWindowGrab(bool _grab);
-		/**
-		Finds context-wide resource groups. I load paths from a config file here,
-		but you can choose your resource locations however you want.
-		*/
-		virtual void locateResources();
-		/**
-		Initialize the RT Shader system.
-		*/
-		bool initialiseRTShaderSystem();
-		/**
-		Loads context-wide resource groups. I chose here to simply initialise all
-		groups, but you can fully load specific ones if you wish.
-		*/
-		virtual void loadResources();
-		void shutdown();
-
-	public:
-		OgreInterface();
-		virtual ~OgreInterface();
-
-		/**
-		This function initializes the render system and resources.
-		*/
-		void initApp();
-		void closeApp();
-	};
-}
+public:
+	OgreInterface(): mRoot(0) {};
+	virtual ~OgreInterface() { delete mRoot; };
+	void initApp();
+};
 
