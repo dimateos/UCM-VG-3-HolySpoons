2dae179ef877de27ebafc5571edaa224b07a7033
Diego BV <diego.baratto8@gmail.com>
2019-02-27 11:46:26 +0100

    Diego Baratto / Juan Barea : Initicialización de Ogre casi completada
    Problemas con los recursos -> tutorial desactualizado
    
    Co-Authored-By: jubarea <jubarea@users.noreply.github.com>

--- Proyecto3/src/Ogre_Interface/OgreInterface.cpp
+++ Proyecto3/src/Ogre_Interface/OgreInterface.cpp
@@ -1,5 +1,6 @@
 #include "OgreInterface.h"
 #include <iostream>
+
 void OgreInterface::createRoot()
 {
 	#if _DEBUG
@@ -7,6 +8,35 @@ void OgreInterface::createRoot()
 	#else
 		mRoot = new Ogre::Root("plugins.cfg");
 	#endif
+}
+
+void OgreInterface::setupResources()
+{
+	Ogre::ConfigFile cf;
+	#if _DEBUG
+		cf.load("resources_d.cfg");
+	#else
+		cf.load("resources.cfg");
+	#endif
+
+	Ogre::String sec, type, arch;
+	Ogre::ConfigFile::SettingsBySection_::const_iterator seci;
+
+	//secciones
+	for (seci = cf.getSettingsBySection().begin(); seci != cf.getSettingsBySection().end(); ++seci)
+	{
+		sec = seci->first;
+		const Ogre::ConfigFile::SettingsMultiMap& settings = seci->second;
+		Ogre::ConfigFile::SettingsMultiMap::const_iterator i;
+
+		//elementos de cada seccion
+		for (i = settings.begin(); i != settings.end(); ++i)
+		{
+			type = i->first;
+			arch = FileSystemLayer::resolveBundlePath(i->second);
+			Ogre::ResourceGroupManager::getSingleton().addResourceLocation(arch, type, sec);
+		}
+	}
 }
 
 bool OgreInterface::setConfiguration()
@@ -18,19 +48,67 @@ bool OgreInterface::setConfiguration()
 
 	mRoot->setRenderSystem(rs);
 	rs->setConfigOption("Full Screen", "No");
-	rs->setConfigOption("Video Mode", "800 x 600 @ 32-bit colour");
+	rs->setConfigOption("Video Mode", "1920 x 1080 @ 32-bit colour");
 
 	return true;
 }
 
 void OgreInterface::createWindow()
 {
-	mWindow = mRoot->initialise(true, "TutorialApplication Render Window");
+	mWindow = mRoot->initialise(true, projectName);
+}
+
+void OgreInterface::initializeResources()
+{
+	Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+}
+
+void OgreInterface::createSceneManager()
+{
+	mSceneMgr = mRoot->createSceneManager();
+}
+
+void OgreInterface::setupScene()
+{
+	//Camara
+	mCamera = mSceneMgr->createCamera("MainCam");
+
+	mCamera->setNearClipDistance(1);
+	mCamera->setFarClipDistance(10000);
+	mCamera->setAutoAspectRatio(true);
+
+	mCamNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("nCam");
+	mCamNode->attachObject(mCamera);
+
+	mCamNode->setPosition(0, 0, 1000);
+	mCamNode->lookAt(Ogre::Vector3(0, 0, 0), Ogre::Node::TS_WORLD);
+
+	//Viewport
+	vp = mWindow->addViewport(mCamera);
+
+	vp->setBackgroundColour(ColourValue(1, 0, 0));
+	mCamera->setAspectRatio(Real(vp->getActualWidth()) / Real(vp->getActualHeight()));
+
+	//Lights
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(.5, .5, .5));
+	mainLight = mSceneMgr->createLight("MainLight");
+	mainLight->setType(Ogre::Light::LT_DIRECTIONAL);
+	mainLight->setDiffuseColour(0.75, 0.75, 0.75);
+	mLightNode = mCamNode->createChildSceneNode("nLuz");
+	mLightNode->attachObject(mainLight);
+
+	mLightNode->setDirection(Ogre::Vector3(0, 0, -1));
+
+	mRoot->startRendering();
 }
 
 void OgreInterface::initApp()
 {
 	createRoot();
+	setupResources();
 	setConfiguration();
 	createWindow();
+	initializeResources();
+	createSceneManager();
+	setupScene();
 }
--- Proyecto3/src/Ogre_Interface/OgreInterface.h
+++ Proyecto3/src/Ogre_Interface/OgreInterface.h
@@ -1,24 +1,50 @@
 #pragma once
-#include <OgreRoot.h>
+#include <OgreFileSystemLayer.h>
+#include <OgreTextureManager.h>
 #include <OgreBuildSettings.h>
+#include <OgreSceneManager.h>
 #include <OgreRenderSystem.h>
 #include "OgreRenderWindow.h"
+#include <OgreConfigFile.h>
+#include <OgreSceneNode.h>
+#include "OgreViewport.h"
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreRoot.h>
 
 using namespace Ogre;
 
-class OgreInterface //: public Ogre::FrameListener
+class OgreInterface
 {
 private:
-	Ogre::Root* mRoot = nullptr;
-	Ogre::RenderWindow* mWindow = nullptr;
+	Root* mRoot = nullptr;
+	RenderWindow* mWindow = nullptr;
 	RenderSystem* rs = nullptr;
+	SceneManager* mSceneMgr = nullptr;
+	Camera* mCamera = nullptr;
+	SceneNode* mCamNode = nullptr;
+	SceneNode* mLightNode = nullptr;
+	Ogre::Light* mainLight = nullptr;
+	Viewport* vp = nullptr;
+	std::string projectName = "Holy Spoons";
 	void createRoot();
+	void setupResources();
 	bool setConfiguration();
 	void createWindow();
+	void initializeResources();
+	void createSceneManager();
+	void setupScene();
 
 public:
 	OgreInterface(): mRoot(0) {};
 	virtual ~OgreInterface() { delete mRoot; };
 	void initApp();
 };
+
+/*
+TODO:
+	*Solucionar inicializar recursos
+	*Object Input (no encuentra los archivos -> no los tenemos)
+	*ESTA CLASE TIENE QUE SER SINGLETON!!!!
+*/
 
