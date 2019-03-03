6e4cb294fdb9ff6baaa0dee7d88b91911be4a74a
Diego BV <diego.baratto8@gmail.com>
2019-03-03 12:36:05 +0100

    Juan Barea / Diego Baratto : Arreglado props
    Co-Authored-By: jubarea <jubarea@users.noreply.github.com>

--- Proyecto3/src/Ogre_Interface/RenderSystemInterface.h
+++ Proyecto3/src/Ogre_Interface/RenderSystemInterface.h
@@ -9,11 +9,14 @@ class RenderSystemInterface
 {
 private:
 	SceneManager * mScnMgr = nullptr;
-public:
-	RenderSystemInterface() {};
+	static RenderSystemInterface* instance_; //singleton pattern
 	RenderSystemInterface(SceneManager * mScnMgr): mScnMgr(mScnMgr){};
 	virtual ~RenderSystemInterface() {};
 
+public:
+	static RenderSystemInterface* createSingleton(SceneManager * mScnMgr);
+	static RenderSystemInterface* getSingleton();
+	void closeInterface();
 	//Resto de interfaz, añadir material, añadir como nodo, shaders (?), añadir como hijo, buscar nodo, camara, luces, viewport...
 };
 
