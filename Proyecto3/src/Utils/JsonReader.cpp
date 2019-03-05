6302ff291f2b36a2a0451bad98cdaf3d10669803
Jorge Rodríguez García <jorgerodrigar2703@gmail.com>
2019-03-05 17:29:55 +0100

    Jorge Rodríguez / Gonzalo Sanz: avanzada la lectura de json. Se lee un nivel que contendrá entidades y cada una de ellas estará definida en Prefabs.json

--- Proyecto3/src/Utils/JsonReader.cpp
+++ Proyecto3/src/Utils/JsonReader.cpp
@@ -1,1 +1,44 @@
 #include "JsonReader.h"
+#include "json.hpp"
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
+
+void JsonReader::ReadLevel(string level) {
+	ifstream i(routeLevel + level + ".json");
+
+	if (i.is_open()) { // Para que no intente abrir archivos que no existen
+
+		json j;
+		i >> j;
+
+		if (!j["GameObjects"].is_null()) {
+			for (int i = 0; i < j["GameObjects"].size(); i++) {
+				if (!j["GameObjects"][i]["Name"].is_null()) {
+					string prefabName = j["GameObjects"][i]["Name"];
+					vector<pair<string, vector<string>>> components;
+					ReadPrefab(prefabName, components);
+				}
+			}
+		}
+	}
+}
+
+void JsonReader::ReadPrefab(string name, vector<pair<string, vector<string>>>& comps) {
+	ifstream i(routePrefabs);
+
+	if (i.is_open()) { // Para que no intente abrir archivos que no existen
+
+		json j;
+		i >> j;
+
+		if (!j[name].is_null()) {
+			for (int i = 0; i < j[name].size(); i++) {
+				if (!j[name][i]["Name"].is_null()) {
+					comps.push_back({ j[name][i]["Name"], j[name][i]["Parameters"] });
+				}
+			}
+		}
+	}
+}
--- Proyecto3/src/Utils/JsonReader.h
+++ Proyecto3/src/Utils/JsonReader.h
@@ -1,12 +1,22 @@
 #ifndef JSON_READER_H_
 #define JSON_READER_H_
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class JsonReader
 {
 private:
+	string routeLevel = "..\\exes\\Assets\\Levels\\";
+	string routePrefabs = "..\\exes\\Assets\\Levels\\Prefabs.json";
+
+	void ReadPrefab(string name, std::vector<pair<string, std::vector<string>>>& comps);
 
 public:
 	JsonReader() {}
+	void ReadLevel(string level);
 	~JsonReader() {}
 };
 
