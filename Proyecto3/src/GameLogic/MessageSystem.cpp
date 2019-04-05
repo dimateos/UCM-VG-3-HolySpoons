9cc72801c4caacf40872fb1e3d0606be04a338e8
Unknown <lluiscan@ucm.es>
2019-04-05 10:15:55 +0200

    Lluis Cañellas : MessageSystem can send messages directly to a GameObject

--- Proyecto3/src/GameLogic/MessageSystem.cpp
+++ Proyecto3/src/GameLogic/MessageSystem.cpp
@@ -82,4 +82,11 @@ void MessageSystem::sendMessageComponentGroup(Message * msg, string name, string
 	}
 }
 
+void MessageSystem::sendMessageGameObject(Message * msg, GameObject * o)
+{
+	for (Component* c : o->getComponents())
+		c->receive(msg);
+}
+
+
 
