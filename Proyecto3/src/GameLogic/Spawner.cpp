0dc41282dd6868dc18c75c9384caaf436ff712d5
JuBarea <jubarea@ucm.es>
2019-04-25 11:44:43 +0200

    juan Barea & Diego Baratto: SmartSpawner
    Co-Authored-By: diegobv <diegobv@users.noreply.github.com>

--- Proyecto3/src/GameLogic/Spawner.cpp
+++ Proyecto3/src/GameLogic/Spawner.cpp
@@ -11,7 +11,8 @@ nap_vector3 Spawner::smartPositioning(GameObject * o)
 	//We get the direction
 	nap_vector3 vec = { trgPos->p_.x_ - o->getTransPtr()->p_.x_, 0, trgPos->p_.z_ - o->getTransPtr()->p_.z_ };
 	//We normalizer the vector to length 1
-	vec.normalize();
+	vec = vec.normalize();
+
 	//We muiltiply the vector by the radius to ensure the item spawns outside the bounding box EJ:5
 	vec *= 5;
 	//We add the vector to our position and return the resulting position
@@ -36,21 +37,24 @@ void Spawner::setUp()
 	pol->setDefault(cfg_["default"]);
 	timer = cfg_["timer"];
 	//We have to read the smart boolean
+	smart = cfg_["smart"];
 
 	pol->init();
 }
 
 void Spawner::update(GameObject * o, double time)
 {
-	lastActiveT += time;
-	if(lastActiveT > timer){					//simple timer
-		lastActiveT = 0;						//Timer reset
-		GameObject* tmp = pol->getItem();		//gets object from pool, spawns it
-		if(!smart)
-			tmp->setPosition(o->getPosition());	//Basic Spawn point
-		else tmp->setPosition(smartPositioning(o));
-		tmp->setActive();
-		MessageSystem::getSingleton()->sendMessageGameObject(&Message(HP_RESET), tmp);
+	if (!o->isKilled()) { //Shoulld not be required
+		lastActiveT += time;
+		if (lastActiveT > timer) {					//simple timer
+			lastActiveT = 0;						//Timer reset
+			GameObject* tmp = pol->getItem();		//gets object from pool, spawns it
+			if (!smart)
+				tmp->setPosition(o->getPosition());	//Basic Spawn point
+			else tmp->setPosition(smartPositioning(o));//Smart spawn point
+			tmp->setActive();
+			MessageSystem::getSingleton()->sendMessageGameObject(&Message(HP_RESET), tmp);
+		}
 	}
 }
 
