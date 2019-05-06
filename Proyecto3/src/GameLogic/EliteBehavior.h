b2b081eec8f9514cecacf23744987b9c0ca682b5
Unknown <lluiscan@ucm.es>
2019-05-06 17:38:56 +0200

    Lluis Cañellas: added elite enemy and some balance, enemies don't fly away

---
+++ Proyecto3/src/GameLogic/EliteBehavior.h
@@ -0,0 +1,47 @@
+//Nap_Time_Studios
+#ifndef ELITEBEHAVIOR_H_
+#define ELITEBEHAVIOR_H_
+
+#include "Component.h"
+#include "Pool.h"
+#include <Timer.h>
+
+
+
+class EliteBehavior :
+	public Component
+{
+protected:
+	enum e_state { move, jump, rocket, shield };
+
+	void moveBehavior(float time);
+	void jumpBehavior(float time);
+	void rocketBehavior(float time);
+	void shieldBehavior(float time);
+	void shoot();
+	virtual void configActive();
+
+	//logic
+	napTimer t;
+	nap_Pool* pRocket,* pBullet,* pSpawn;
+
+	nap_transform* dest;
+	vector<nap_vector3> positions, directions;
+
+	//cfg (json)
+	float speed = 0.1, actionDelay = 2, shotSpeed = 50, lowY = 2, highY = 10, baseY = lowY;
+	int i = 0, maxBullets = 3, maxJumps = 2, lastAttack = 3;
+	e_state status = move;
+	bool up = true;
+
+public:
+
+	inline EliteBehavior(nap_json const & cfg, GameObject* owner) : Component(cfg, owner) {};
+	virtual ~EliteBehavior() {};
+
+	virtual void setUp();
+
+	virtual void update(GameObject* ent, double time);
+};
+
+#endif /* ELITEBEHAVIOR_H_ */
